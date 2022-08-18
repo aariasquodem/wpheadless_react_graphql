a5b757124a73736f73fd910d22e3cdc48d48a66d
Alvaro Arias <aarias@quodem.com>
2022-08-18 12:11:10 +0200

    check if username already exists

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import {db, auth} from '../../firebase';
-import { setDoc, doc } from "firebase/firestore";
+import { setDoc, doc, getDocs, collection, query, where } from "firebase/firestore";
 import {createUserWithEmailAndPassword} from "firebase/auth";
 
 const SignUp = () => {
@@ -11,24 +11,35 @@ const SignUp = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const username = e.target.username.value;
+    const username = e.target.username.value.toLowerCase();
     const email = e.target.email.value;
     const password = e.target.pass.value;
     const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*\_\-])(?=.{8,})/;
     if(regexPassword.test(password)){
       try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-        await setDoc(doc(db, "users", user.uid), {
-          'username': username,
-          'email': email,
-          'favs': []
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, where("username", "==", username));
+        const querySnapshot = await getDocs(q);
+        let result
+        querySnapshot.forEach((doc) => {
+          result = doc.data().username;
         });
-        setError('');
-        navigate('/');
+        if(!result){
+          const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+          const user = userCredential.user;
+          await setDoc(doc(db, "users", user.uid), {
+            'username': username,
+            'email': email,
+            'favs': []
+          });
+          setError('');
+          navigate('/');
+        } else {
+          setError('A user with that username already exists');
+        }
       } catch (error) {
         console.log('Error:', error);
-        setError('Invalid user or password');
+        setError('Invalid username or password');
       }
     } else{
       setError('Password must contain at least 8 alphanumeric characters, 1 uppercase letter and a special character');
